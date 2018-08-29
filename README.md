f22ed18b06528c41707da812201697d4c7563896
Joe Janiga <josephjaniga@gmail.com>
2018-08-29 16:56:15 -0400

    updates to the project as a whole with some improvements

---
+++ README.md
@@ -0,0 +1,118 @@
+# ino
+
+A CLI wrapper written in NodeJS for the `arduino-cli` go application, with the intention of making the CLI easier to use.
+
+## Features
+
+* Select a board to "activate" it, and the remaining commands will use that board.
+* Will identify the board by its unique ID, if ports change it will identify where the board is and work with it.
+
+## Requirements
+
+* Expects Arduino IDE 1.6.x or greater to be installed on your system
+* Expects `arduino-cli` to be available to the command line
+* Requires NodeJS 8.x.x
+
+---
+
+## Usage
+
+```
+$ ino -h
+
+  Usage: ino [options] [command]
+
+  Options:
+
+    -V, --version         output the version number
+    -h, --help            output usage information
+
+  Commands:
+
+    boards                Lists the attached boards
+    select <boardNumber>  Select a board to set as the active board
+    compile               Compile a sketch for the active board
+    upload                Upload a sketch to the active board
+
+```
+
+---
+
+### `ino boards`
+
+This command will list the boards connected to the system, each identified on the left with a Unique Zero Based Index 0-N.  Note plugging boards, unplugging boards and uploading sketches to boards will change the board connections to the system.
+
+```
+$ ino boards
+# - FBQN                PORT    ID              NAME
+0 -                     COM3    1A86:7523       unknown
+1 - arduino:avr:micro   COM4    2341:8037       Arduino/Genuino Micro
+```
+
+---
+
+### `ino select <boardNumber>`
+
+* &lt;boardNumber&gt; required - should be an integer from 0 - N representing the index displayed from the `ino boards` command
+
+This command will set the selected board to active, effectively saving it for use with further `ino` commands.
+
+```
+$ ino select 1
+
+Selected Board 1
+
+FQBN: arduino:avr:micro
+id: 2341:8037
+name: Arduino/Genuino Micro
+port: COM4
+```
+
+---
+
+### `ino compile [sketch]`
+
+* [sketch] optional - a Path to the sketch you are trying to compile.  If this path is not provided, will default to the current working directory.
+
+Compile the provided sketch for the active board.
+
+```
+$ ino compile /c/Path/To/Your/Sketch
+Attempting to compile /c/Path/To/Your/Sketch for board:
+
+FQBN: arduino:avr:micro
+id: 2341:8037
+name: Arduino/Genuino Micro
+port: COM4
+
+Sketch uses 4130 bytes (14%) of program storage space. Maximum is 28672 bytes.
+Global variables use 149 bytes (5%) of dynamic memory, leaving 2411 bytes for local variables. Maximum is 2560
+bytes.
+```
+
+---
+
+### `ino upload [sketch]`
+
+* [sketch] optional - a Path to the sketch you are trying to compile.  If this path is not provided, will default to the current working directory.
+
+Upload a compiled sketch for the active board.
+
+```
+$ ino upload /c/Path/To/Your/Sketch
+Attempting to upload /c/Path/To/Your/Sketch to board:
+
+FQBN: arduino:avr:micro
+id: 2341:8037
+name: Arduino/Genuino Micro
+port: COM4
+
+Connecting to programmer: .
+Found programmer: Id = "CATERIN"; type = S
+    Software Version = 1.0; No Hardware Version given.
+Programmer supports auto addr increment.
+Programmer supports buffered memory access with buffersize=128 bytes.
+
+Programmer supports the following devices:
+    Device code: 0x44
+```
---
+++ ino.js
@@ -0,0 +1,205 @@
+#!/usr/bin/env node
+
+/**
+ * Operate on boards by the board ID... not numeric index
+ */
+
+'use strict';
+
+// constants
+const HOME_DIR = require('os').homedir();
+const INO_DIR = `${HOME_DIR}/.ino`;
+
+const BOARD_DB_FILE = `${INO_DIR}/boards.json`;
+const ACTIVE_BOARD_FILE = `${INO_DIR}/active.json`;
+
+// libs
+const program = require('commander');
+const execSync = require('child_process').execSync;
+const fs = require('fs');
+
+if (!fs.existsSync(INO_DIR)){
+    fs.mkdirSync(INO_DIR);
+}
+
+program.version('0.0.1');
+
+program
+    .command('boards')
+    .description('Lists the attached boards')
+    // .option()
+    .action(boardsAction);
+
+program
+    .command('select <boardNumber>')
+    .description('Select a board to set as the active board')
+    .action(selectAction);
+
+program
+    .command('compile [sketch]')
+    .description('Compile a sketch for the active board.  If no sketch path is provided will attempt to use the current working directory.')
+    .action(compileAction);
+
+program
+    .command('upload [sketch]')
+    .description('Upload a sketch to the active board.  If no sketch path is provided will attempt to use the current working directory.')
+    .action(uploadAction);
+
+function boardsAction (cmd) {
+
+    let boardList = getFormattedBoardListFromArduino();
+
+    displayBoardList(boardList);
+
+    if (fs.existsSync(BOARD_DB_FILE)){
+        fs.truncateSync(BOARD_DB_FILE);
+    }
+
+    fs.writeFileSync(BOARD_DB_FILE, JSON.stringify(boardList));
+}
+
+function selectAction (boardNumber, cmd) {
+
+    if (fs.existsSync(ACTIVE_BOARD_FILE)){
+        fs.truncateSync(ACTIVE_BOARD_FILE);
+    }
+
+    let boards = JSON.parse(fs.readFileSync(BOARD_DB_FILE));
+    let board = boards[boardNumber];
+
+    console.log(`\nSelected Board ${board["displayIndex"]}\n`);
+    logBoard(board);
+
+    fs.writeFileSync(ACTIVE_BOARD_FILE, JSON.stringify(board));
+
+}
+
+function compileAction (sketch, cmd) {
+
+    if (!sketch) {
+        sketch = process.cwd();
+    }
+
+    let activeBoard = getActiveBoard();
+    let foundBoard = findBoardById(activeBoard.id);
+
+    if (activeBoard.port != foundBoard.port) {
+        console.log("The Active board has changed.  Found it by unique ID.")
+    }
+
+    console.log(`Attempting to compile ${sketch} for board:\n`);
+    logBoard(foundBoard);
+
+    let command = `arduino-cli compile --fqbn ${foundBoard.fqbn} ${sketch}`;
+    let arduinoCommandOutput = execSync(command);
+
+    console.log(arduinoCommandOutput.toString());
+
+}
+
+function uploadAction (sketch, cmd) {
+
+    if (!sketch) {
+        sketch = process.cwd();
+    }
+
+    let activeBoard = getActiveBoard();
+    let foundBoard = findBoardById(activeBoard.id);
+
+    console.log(`Attempting to upload ${sketch} to board:\n`);
+    logBoard(foundBoard);
+
+    let command = `arduino-cli upload -p ${foundBoard.port} --fqbn ${foundBoard.fqbn} ${sketch}`
+    let arduinoCommandOutput = execSync(command);
+
+    console.log(arduinoCommandOutput.toString());
+}
+
+function getActiveBoard () {
+    let board = JSON.parse(fs.readFileSync(ACTIVE_BOARD_FILE));
+    return board;
+}
+
+function getFormattedBoardListFromArduino () {
+
+    let arduinoCommandOutput = execSync(`arduino-cli board list`);
+    let outputString = arduinoCommandOutput.toString();
+
+    let index = outputString.indexOf('FQBN');
+    outputString = outputString.slice(index);
+
+    let boardList = outputString.split('\n');
+    boardList = boardList.filter(n => n !== '');
+
+    let boards = boardList.slice(1);
+
+    boardList = [];
+
+    let displayIndex = 0;
+
+    for (let board of boards) {
+        let temp = board.split('\t');
+
+        boardList.push({
+            displayIndex,
+            fqbn: temp[0].trim(),
+            port: temp[1].trim(),
+            id: temp[2].trim(),
+            name: temp[3].trim()
+        });
+
+        displayIndex++;
+    }
+
+    return boardList;
+}
+
+function displayBoardList (boardList) {
+
+    let fqbnLabel = pad('                      ', 'FQBN'),
+        portLabel = pad('              ', 'PORT'),
+        idLabel = pad('            ', 'ID'),
+        nameLabel = pad('                              ', 'NAME');
+
+    console.log(`# - ${fqbnLabel}${portLabel}${idLabel}${nameLabel}`);
+
+    for (let board of boardList) {
+
+        let fqbn = pad('                      ', board.fqbn),
+            port = pad('              ', board.port),
+            id = pad('            ', board.id),
+            name = pad('                              ', board.name);
+
+        console.log(`${board.displayIndex} - ${fqbn}${port}${id}${name}`);
+    }
+
+}
+
+function findBoardById (boardId) {
+
+    let boardList = getFormattedBoardListFromArduino();
+
+    let board = boardList[boardList.findIndex(board => board.id === boardId)];
+
+    return board;
+
+}
+
+function logBoard (board) {
+    console.log(`fqbn: ${board["fqbn"]}`);
+    console.log(`id: ${board["id"]}`);
+    console.log(`name: ${board["name"]}`);
+    console.log(`port: ${board["port"]}\n`);
+}
+
+function pad(pad, str, padLeft) {
+    if (typeof str === 'undefined')
+        return pad;
+    if (padLeft) {
+        return (pad + str).slice(-pad.length);
+    } else {
+        return (str + pad).substring(0, pad.length);
+    }
+}
+
+program.parse(process.argv);
